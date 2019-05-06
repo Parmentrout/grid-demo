e1a37f667599a0d42281d00ba7155e285580bd0e
Patrick Armentrout <patrick.armentrout@gmail.com>
2019-05-06 12:12:35 -0600

    Add synchfusion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,18 +10,21 @@ import { MatSortModule } from '@angular/material/sort';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AgGridModule } from 'ag-grid-angular';
 import { AgGridDemoComponent } from './ag-grid-demo/ag-grid-demo.component';
+import { GridModule } from '@syncfusion/ej2-angular-grids';
+import { SyncfusionComponent } from './syncfusion/syncfusion.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
+    GridModule,
     MatTableModule,
     MatSortModule,
     MatTabsModule,
     AgGridModule.withComponents([])
   ],
-  declarations: [AppComponent, VSSGridComponent, AgGridDemoComponent],
+  declarations: [AppComponent, VSSGridComponent, AgGridDemoComponent, SyncfusionComponent],
   providers: [DataService],
   bootstrap: [AppComponent]
 })
